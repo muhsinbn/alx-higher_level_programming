9abafc6f9daeb9f160362dc3d6cac79fcbeefd65
Mohammed <muhsinbnyahya@gmail.com>
2024-01-25 06:54:27 -0800

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-def update_dictionary(a_dictionary, key, value):
+def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
-        a_dictionary[key] = value
-    else:
-        a_dictionary[key] = value
+        del a_dictionary[key]
     return a_dictionary
