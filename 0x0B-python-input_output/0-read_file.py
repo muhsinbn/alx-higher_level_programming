ab732ac6974d3ca05519d207b88cbddaed4148f0
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-02-20 06:12:20 -0800

    task_0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def read_file(filename):
+    """
+    Read the contents of the file and print them to stdout
+    """
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end='')
