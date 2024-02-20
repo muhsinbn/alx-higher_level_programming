3c0e058ae08db1cd4d792f448edb75d3de569f8c
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-02-20 14:26:01 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 
-def read_file(filename):
+"""
+Using the UTF8 way, read a text file.
+Print to stdout.
+Prototype: def read_file(filename=""):
+You must use the with statement
+You don’t need to manage file permission or file doesn't exist exceptions.
+"""
+
+
+def read_file(filename=""):
     """
-    Read the contents of the file and print them to stdout
+    Open and read a UTF8 file and print to stdout.
+    Args:
+        filename
     """
-    with open(filename, encoding='utf-8') as file:
-        text = file.read()
-        print(text, end='')
+    with open(filename, encoding="UTF8") as myfile:
+        text = myfile.read()
+        print(text, end="")
