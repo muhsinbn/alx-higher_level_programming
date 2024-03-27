3bfcd94f607ae8443280daa8fa7ff33c70b891eb
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-03-27 01:24:37 -0700

    code

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,9 @@
+-- Disable foreign key checks
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
+
+-- Enable foreign key checks
+SET FOREIGN_KEY_CHECKS = 1;
+
