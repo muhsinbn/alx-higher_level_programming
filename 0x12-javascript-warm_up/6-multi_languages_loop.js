fe870da41c1a02cec292c5aed3148509c109e820
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-04-22 07:26:43 -0700

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Define an array of strings
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Loop through the array and print each string
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
