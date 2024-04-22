390788181310bfb4e762ee2f93c056fa9ace7cbe
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-04-22 07:05:22 -0700

    code

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Destructure the third element of process.argv to get the argument
+const [,, arg] = process.argv;
+
+// Check if an argument was passed and print it, or print "No argument"
+if (arg === undefined) {
+    console.log('No argument');
+} else {
+    console.log(arg);
+}
