3c772625d06ff0cbca8ce62212a5268751993d14
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-04-23 01:06:46 +0100

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// This script prints "C is fun" x times, where x is the first argument
+
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+    console.log("Missing number of occurrences");
+} else {
+    let count = 0;
+    while (count < x) {
+        console.log("C is fun");
+        count++;
+    }
+}
