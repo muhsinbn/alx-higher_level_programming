90179251c3b45c67824439c528fb7ae0b79e4775
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-04-23 14:28:56 +0100

    code

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
 
 // Class definition for Rectangle with width and height attributes
-class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+module.exports = class Rectangle {
+    constructor (w, h) {
+        if (w > 0 && h > 0) {
             this.width = w;
             this.height = h;
-        } else {
-            // Create an empty object if w or h is 0 or not a positive integer
-            this.width = 0;
-            this.height = 0;
         }
     }
-}
-
-module.exports = Rectangle;
+};
