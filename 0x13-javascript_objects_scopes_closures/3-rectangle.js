e06a09cdfed719aa309f65212ae691136d8f1304
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-04-24 08:04:51 +0100

    code

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,16 +2,16 @@
 
 // Class definition for Rectangle with width and height attributes
 class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    // Instance method to print the rectangle using the character 'X'
-    print() {
-        if (this.width === 0 || this.height === 0) {
+  // Instance method to print the rectangle using the character 'X'
+  print () {
+    if (this.width === 0 || this.height === 0) {
             console.log('');
         } else {
             for (let i = 0; i < this.height; i++) {
