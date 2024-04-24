e6c8a3e35d873ded92edd9180f858c625d01b63b
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-04-24 00:31:57 -0700

    code

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 
-// esrever.js
-// Function that returns the reversed version of a list
 exports.esrever = function(list) {
-	const reversedList = [];
-	for (let i = list.length - 1; i >= 0; i--) {
-	  reversedList.push(list[i]);
-  }
-	return reversedList;
+    const reversedList = [];
+    for (let i = list.length - 1; i >= 0; i--) {
+      reversedList.push(list[i]);
+    }
+    return reversedList;
 };
