0e96ab65feda21dd9e9e3a0c545e7e4f721e02d5
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-04-26 00:11:56 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,22 @@
-import MySQLdb
 import sys
+import MySQLdb
+
+def list_states(username, password, database):
+    with MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database) as db:
+        with db.cursor() as cursor:
+            cursor.execute("SELECT * FROM states ORDER BY id ASC")
+            states = cursor.fetchall()
+
+            for state in states:
+                print(state)
 
 if __name__ == "__main__":
-    # Check if the correct number of argument are provided
-    if len(sys.arg) != 4:
-        print("Usage: python script.py <mysql_username> <mysql_password> <database_name")
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")
         sys.exit(1)
 
-        # Get MySQL username, password, and database name from the argument
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-        # Connect to the MySQL server and retrieve states data
-        db = MySQLdb.connect(host="localhost", port=3306, user=username passwd=password, db=database)
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        states = cursor.fetchall()
-
-        # Display the states data
-        for state in states:
-            print(state)
-
-            # Close cursor and database connection
-            cursor.close()
-            db.close()
+    list_states(username, password, database)
